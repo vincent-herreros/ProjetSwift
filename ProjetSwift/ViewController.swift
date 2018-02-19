6375201382bd5f2b47d7f851d5b4a27315d0d3f5
Vincent HERREROS <vincent.herreros@igtp3-mac14.isim.intra>
2018-02-19 16:16:50 +0100

    Initial Commit

---
+++ ProjetSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjetSwift
+//
+//  Created by Vincent HERREROS on 19/02/2018.
+//  Copyright © 2018 Vincent HERREROS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
