b53696b3107991e7295f1994e1635b785962cf9c
Sheena Maucuer <sheena@sheenas-macbook-pro.local>
2018-02-26 22:07:21 +0100

    Mise en place des boutons et changement de page

---
+++ ProjetSwift/MedicamentController.swift
@@ -0,0 +1,26 @@
+//
+//  MedicamentController.swift
+//  ProjetSwift
+//
+//  Created by Sheena Maucuer on 26/02/2018.
+//  Copyright © 2018 Vincent HERREROS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MedicamentController : UIViewController{
+    
+    var medicament : Medicament?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ ProjetSwift/MedicamentViewController.h
@@ -0,0 +1,13 @@
+//
+//  MedicamentViewController.h
+//  ProjetSwift
+//
+//  Created by Sheena Maucuer on 26/02/2018.
+//  Copyright © 2018 Vincent HERREROS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MedicamentViewController : NSObject
+
+@end
