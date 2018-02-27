aed06e49c3d825725031957dbc8c47648ad5d22a
Sheena Maucuer <sheena@sheenas-macbook-pro.local>
2018-02-27 01:36:21 +0100

    création donnée persistante (test) + lien nouvelle fenêtre (test)

---
+++ ProjetSwift/MedicamentViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MedicamentViewController.swift
+//  ProjetSwift
+//
+//  Created by Sheena Maucuer on 27/02/2018.
+//  Copyright © 2018 Vincent HERREROS. All rights reserved.
+//
+
+import UIKit
+
+class MedicamentViewController: UIViewController {
+
+    @IBOutlet weak var labelMedoc: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
